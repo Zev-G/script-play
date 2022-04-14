7582d299caa59ac377a76f3abb68495a2cc097d4
Zev <vezvez1234@gmail.com>
2022-04-14 11:27:33 -0700

    removed unneeded code

--- src/App.svelte
+++ src/App.svelte
@@ -2,21 +2,8 @@
     import { script, ScriptElement } from "./TextProcessing";
     import process from "./TextProcessing";
     import ScriptView from "./ScriptView.svelte";
-    import { onMount } from "svelte";
 
     const elements: ScriptElement[] = process(script);
-
-    onMount(() => {
-        document.onkeydown = keyDown;
-    });
-
-    function keyDown(event) {
-        if (event.code == "ArrowLeft") {
-            console.log("up");
-        } else if (event.code == "ArrowRight") {
-            console.log("down");
-        }
-    }
     
 </script>
 
